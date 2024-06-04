fd5b6663db4ebb980bf0ce49d1b06a4b95b65e4a
DenysChaykovskyy <dickchest@gmail.com>
2024-06-04 20:13:18 +0200

    Initial commit

---
+++ src/main/java/com/match/MatchApp2Application.java
@@ -0,0 +1,13 @@
+package com.match;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchApp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/match/MatchApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.match;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
