1652ac5280c1bd095024349abcff21b6865273d7
DenysChaykovskyy <dickchest@gmail.com>
2024-06-05 18:51:21 +0200

    TelegramAuthData.java

--- src/main/java/com/match/services/TelegramAuthService.java
+++ src/main/java/com/match/services/TelegramAuthService.java
@@ -1,14 +1,17 @@
 package com.match.services;
 
 import com.match.domain.TelegramAuthData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TelegramAuthService {
 
+    // значение botToken будет установлено из конфигурационного файла или переменной окружения
     private final String botToken;
 
-    public TelegramAuthService(String botToken) {
+    public TelegramAuthService(@Value("${telegram.bot-token}") String botToken) {
         this.botToken = botToken;
     }
 
@@ -16,5 +19,4 @@ public class TelegramAuthService {
         // Вызов метода checkAuth для проверки хеша
         return telegramAuthData.checkAuth(botToken);
     }
-
 }
