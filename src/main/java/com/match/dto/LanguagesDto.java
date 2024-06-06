d04c04512e5989fd9fdf4350a6ff7665cd5ba85a
DenysChaykovskyy <dickchest@gmail.com>
2024-06-06 23:17:12 +0200

    UserProfileDto.java

---
+++ src/main/java/com/match/dto/LanguagesDto.java
@@ -0,0 +1,11 @@
+package com.match.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LanguagesDto {
+    private String name;
+    private String logo;
+}
--- src/main/java/com/match/dto/UserProfileDto.java
+++ src/main/java/com/match/dto/UserProfileDto.java
@@ -1,30 +1,29 @@
-package com.match.domain;
+package com.match.dto;
 
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
 @Getter
 @Setter
-public class UserProfile {
+public class UserProfileDto {
     private String id;
     private String userId;
     private String firstName;
     private String secondName;
     private String userName;
-    private LocalDate dateOfBirth;
+    private String dateOfBirth; // Для DTO используем строковое представление даты
     private Integer gender;
     private String city;
-    private Languages languages;
+    private LanguagesDto languages; // Отдельный DTO для languages, если это не Enum
     private Map<Integer, Integer> goals;
     private Boolean goalsShow;
     private List<Integer> genderLook;
     private Boolean genderLookShow;
     private List<String> values;
-    private UserProfileQuestions questions;
+    private UserProfileQuestionsDto questions; // Отдельный DTO для questions
     private Map<String, Integer> toddlers;
     private List<String> interests;
     private List<String> photos;
